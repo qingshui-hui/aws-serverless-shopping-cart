cb33a8fbe0f37777fd27c6594da8eb95d76e0438
qingshui-hui <stakanari987@gmail.com>
2022-03-09 07:28:01 +0900

    create lambda handler test

--- backend/shopping-cart-service/tests/test_get_cart_total.py
+++ backend/shopping-cart-service/tests/test_get_cart_total.py
@@ -1,6 +1,14 @@
+import json
+import os
+import sys
+
 import boto3
 import pytest
-from moto import mock_dynamodb2
+from moto import mock_dynamodb2, mock_lambda
+
+from mock import patch
+
+sys.path.append("..")
 
 from .mock import get_mock_table, sample_data
 
@@ -23,3 +31,27 @@ class TestGetCartTotal:
         # 1件も見つからなかったらItemがセットされない
         assert "Item" in response
         assert response["Item"]["pk"] == "product#d2580eff-d105-45a5-9b21-ba61995bc6da"
+
+    @mock_dynamodb2
+    @mock_lambda
+    def test_handler(self):
+        with patch.dict(os.environ, {'TABLE_NAME': 'sample_table'}):
+            from get_cart_total import lambda_handler
+        mock_table = get_mock_table()
+        mock_table.put_item(Item={
+            "pk": "product#d2580eff-d105-45a5-9b21-ba61995bc6da",
+            "sk": "totalquantity",
+            "quantity": 1,
+        })
+        event = {
+            "pathParameters": {
+                "product_id": "d2580eff-d105-45a5-9b21-ba61995bc6da"
+            }
+        }
+        res = lambda_handler(event, Context())
+        print(res['body'])
+        parsedBody = json.loads(res['body'])
+        assert parsedBody["product"] == "d2580eff-d105-45a5-9b21-ba61995bc6da"
+
+class Context:
+    pass
